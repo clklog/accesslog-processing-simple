ef4838e4f2558c7d81036f89728da247687a3f09
Pocketpc <pongpongcn@gmail.com>
2025-12-31 10:41:03 +0800

    Refactor SQL schema for access log database
    Signed-off-by: Pocketpc <pongpongcn@gmail.com>

--- sql/init.sql
+++ sql/init.sql
@@ -1,53 +1,47 @@
-CREATE DATABASE accesslogdb ENGINE = Atomic;
-
-CREATE TABLE accesslogdb.gp_nginx_access
+CREATE TABLE IF NOT EXISTS gp_nginx_access
 (
-    `country` String,
-    `upstream_uri` String,
-    `upstream_addr` String,
-    `uri` String,
-    `request_method` String,
-    `http_host` String,
-    `http_user_agent` String,
-    `stat_hour` String,
-    `manufacturer` String,
-    `remote_user` String,
-    `upstream_status` String,
-    `request_time` Float32,
-    `province` String,
-    `browser` String,
-    `model` String,
-    `browser_version` String,
-    `brand` String,
-    `remote_addr` String,
-    `stat_date` Date,
-    `stat_min` String,
-    `time_local` DateTime64(3),
-    `http_version` String,
-    `city` String,
-    `body_sent_bytes` String,
-    `http_referrer` String,
-    `server_name` String,
-    `upstream_response_time` String,
-    `status` String,
-    `application_code` String,
-    `create_time` DateTime64(3) DEFAULT now(),
-    `raw_uri` String
+    country LowCardinality(String),
+    upstream_uri String,
+    upstream_addr LowCardinality(String),
+    uri String,
+    request_method LowCardinality(String),
+    http_host LowCardinality(String),
+    http_user_agent String,
+    stat_hour String,
+    manufacturer String,
+    remote_user String,
+    upstream_status LowCardinality(String),
+    request_time Float32,
+    province String,
+    browser String,
+    model String,
+    browser_version String,
+    brand String,
+    remote_addr String,
+    stat_date Date,
+    stat_min String,
+    time_local DateTime64(3),
+    http_version LowCardinality(String),
+    city String,
+    body_sent_bytes UInt64,
+    http_referrer String,
+    server_name LowCardinality(String),
+    upstream_response_time String,
+    status UInt16,
+    application_code LowCardinality(String),
+    create_time DateTime64(3) DEFAULT now64(3),
+    raw_uri String
 )
-    ENGINE = MergeTree
+ENGINE = MergeTree
 PARTITION BY stat_date
-ORDER BY (application_code, http_host, status, request_method, remote_addr, country, province, city, uri, browser, time_local)
-SETTINGS index_granularity = 8192;
+ORDER BY (application_code, http_host, time_local, uri);
 
-
-CREATE TABLE accesslogdb.tbl_app
+CREATE TABLE IF NOT EXISTS tbl_app
 (
-    `app_code` String,
-    `app_name` String,
-    `owner` String,
-    `create_time` DateTime64(3)
+    app_code String,
+    app_name String,
+    owner String,
+    create_time DateTime64(3)
 )
-    ENGINE = MergeTree
-PARTITION BY app_code
-ORDER BY create_time
-SETTINGS index_granularity = 8192;
+ENGINE = ReplacingMergeTree
+ORDER BY app_code;
